d444ef1486cf723671774efbafdd66f355fed072
lemstanislav5 <lemstanislav5@gmail.com>
2023-07-06 22:28:33 +0300

    111

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import TextControlsExample from './components/pages/authorization/Authorization'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <><TextControlsExample/></>
-  
+    <Container fluid>
+      <Row>
+      <Col xs={3}>1 of 3</Col>
+        <Col xs={9}><TextControlsExample/></Col>
+      </Row>
+    </Container>  
   );
 }
 

57cbca2a3fe808fd2b664aeb4b53e8848e18db13
lemstanislav5 <lemstanislav5@gmail.com>
2024-01-06 23:57:08 +0300

    setings

---
+++ src/components/pages/setings/Setings.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AuthService from "../../../api/auth";
+import { Navigate } from "react-router-dom";
+import Alert from 'react-bootstrap/Alert';
+
+export default function Stings() {
+  return (
+    <Row className="justify-content-md-center" >
+      <Col xs={4}>
+       77676769876
+      </Col>
+    </Row>
+  );
+}

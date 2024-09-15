18804cb78511b24a50551022456aea8fdc0bde43
lemstanislav5 <lemstanislav5@gmail.com>
2024-09-15 19:46:47 +0300

    1111

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -11,7 +11,11 @@ function Menu() {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">Админпанель</Navbar.Brand>
+          <Navbar.Brand href="/" style={{paddingBottom: "9px"}}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2z"/>
+          </svg>
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/messages"><BsChatRightText /> Сообщения</Nav.Link>
             <Nav.Link href="/setings"><BsGear /> Настройки</Nav.Link>
